7fda736a4ec9dacbd3f7c1e62cd8e335161cf3ab
Dmytro Kovalenko <dimitri.yu.kovalenko@gmail.com>
2023-04-09 22:09:10 +0300

    echo bot

---
+++ main.py
@@ -0,0 +1,32 @@
+"""
+This is a echo bot.
+It echoes any incoming text messages.
+"""
+
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+from config import TOKEN
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
