2a3980e6f9a17a9b97ea304ddddf05bea5bacf28
Dmytro Kovalenko <dimitri.yu.kovalenko@gmail.com>
2023-04-10 14:47:12 +0300

    bitcoin echo bot :)

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ It echoes any incoming text messages.
 """
 
 import logging
+import json
 from aiogram import Bot, Dispatcher, executor, types
+from binance.client import Client
 
-from config import TOKEN
+from config import TOKEN, SECRET_KEY, API_KEY
 
 
 # Configure logging
@@ -15,7 +17,7 @@ logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-
+client = Client(API_KEY, SECRET_KEY)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
@@ -25,7 +27,8 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await message.answer(message.text)
+    btc_price_json = client.get_symbol_ticker(symbol="BTCUSDT")
+    await message.answer(f"Bitcoin costs {btc_price_json['price']} USDT")
 
 
 if __name__ == '__main__':
