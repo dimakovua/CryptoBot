71e306ab2047bd5fcccb62b411170ae1f0072d92
u <u@rt-130.local>
2023-04-11 12:15:01 +0300

    balance test 15

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ async def echo(message: types.Message):
     result_string = ""
     for symbol, price in crypto_prices.items():
         result_string += f"{price} {symbol}\n"
-    result_string += f"{usdt_balance['free']} USDT"
+    result_string += f"{usdt_balance['free']} USDT\n"
     result_string += f"All balance equivalent in BTC - {sum_btc} == {own_usd} USDT"
     #await message.answer(f"* Spot => {sum_btc} BTC == {own_usd} USDT ", reply_markup=main_kb)
     await message.answer(result_string, reply_markup=main_kb)
